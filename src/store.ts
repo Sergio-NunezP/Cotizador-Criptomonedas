b38030b17192ee98fa04df0dc5d18d69938b9fef
Sergio Núñez <sergioop722@gmail.com>
2025-02-28 08:56:58 -0500

    para no tener un store tan pesado se mueve la funcion que obtiene la API hacia otro archivo

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,7 @@
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
-import axios from 'axios'
-import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema'
 import { Cryptocurrency } from './types'
+import { getCryptos } from './services/CryptoService'
 
 type CryptoStore = {
     cryptocurrencies: Cryptocurrency[]
@@ -10,16 +9,7 @@ type CryptoStore = {
 
 }
 
-async function getCryptos() {
-    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
-    const { data: { Data } } = await axios(url)
-    const result = CryptoCurrenciesResponseSchema.safeParse(Data)
-    if (result.success) {
-        return result.data
-    }
-
-}
-
+// Mostrar las criptos en el state con devtools
 export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
     cryptocurrencies: [],
     fetchCryptos: async () => {
